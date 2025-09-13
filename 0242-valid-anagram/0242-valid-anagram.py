1c3b3c440f396600f01d465d6093ebac401f5bdb
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-09-13 17:43:08 -0600

    Problem:0242-valid-anagram - Easy - BigO[7 ms (89.19%)-17.9 MB (64.77%)] -.py

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,11 @@
+from collections import Counter
 class Solution:
+
+    from collections import Counter
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
-        
+        h=Counter(s)
+        hh=Counter(t)
+        if h==hh:
+            return True
+        else:
+            return False
